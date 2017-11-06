60652462e313880e6fd65ea84107ce4a7b5412db
Student <Student@PC80>
2017-11-06 10:29:56 +0800

    Agile Practical 4 Lab

---
+++ src/practical_4/agile/Practical_4Agile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical_4.agile;
+
+/**
+ *
+ * @author Student
+ */
+public class Practical_4Agile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
